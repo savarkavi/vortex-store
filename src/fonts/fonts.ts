976101e0735e71d1051abe106efffae391226be7
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-03-22 14:38:36 +0530

    Add Share Tech Mono font import to fonts.ts

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -1,5 +1,11 @@
 import localFont from "next/font/local";
+import { Share_Tech_Mono } from "next/font/google";
 
 export const duneFont = localFont({
   src: "./Dune_Rise.otf",
+});
+
+export const shareTechMono = Share_Tech_Mono({
+  subsets: ["latin"],
+  weight: ["400"],
 });
