23c5732dcde73ab18aa82476128ac0c9e6f96df7
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-03-22 14:39:00 +0530

    Update Hero component layout for improved responsiveness and styling

--- src/components/home-page/Hero.tsx
+++ src/components/home-page/Hero.tsx
@@ -4,9 +4,9 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="relative w-full h-full flex flex-col items-center">
+    <div className="relative flex h-screen w-full flex-col items-center">
       <h1 className={`${duneFont.className} text-[18rem]`}>VORTEX</h1>
-      <div className="absolute top-1/2 left-1/2 -translate-1/2 -translate-y-1/2 w-full h-full">
+      <div className="absolute top-1/2 left-1/2 h-full w-full -translate-1/2 -translate-y-1/2">
         <Image
           src="/hero-img.png"
           alt="hero main image"
