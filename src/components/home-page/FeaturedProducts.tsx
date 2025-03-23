b559ac5d171d4abad66410db442db56b08df63bd
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-03-23 14:47:47 +0530

    Refactor FeaturedProducts component to use Wix API for fetching collections and products

--- src/components/home-page/FeaturedProducts.tsx
+++ src/components/home-page/FeaturedProducts.tsx
@@ -1,21 +1,18 @@
-import { getWixClient } from "@/lib/wix-client.base";
 import FeaturedProductCard from "./FeaturedProductCard";
+import { fetchCollectionBySlug } from "@/wix-api/collections";
+import { fetchProductsByCollection } from "@/wix-api/products";
 
 const FeaturedProducts = async () => {
-  const wixClient = getWixClient();
+  const collection = await fetchCollectionBySlug("featured-products");
 
-  const { collection } =
-    await wixClient.collections.getCollectionBySlug("featured-products");
+  if (!collection || !collection._id) return null;
 
-  if (!collection) return null;
-
-  const products = await wixClient.products
-    .queryProducts()
-    .hasSome("collectionIds", [collection._id])
-    .find();
+  const products = await fetchProductsByCollection({
+    collectionIds: collection._id,
+  });
 
   return (
-    <div className="mt-16 grid h-full w-full max-w-[1400px] grid-cols-3 justify-items-center gap-4">
+    <div className="mt-16 grid h-full w-full max-w-[1400px] grid-cols-1 justify-items-center gap-4 sm:grid-cols-2 lg:grid-cols-3">
       {products.items.map((item) => (
         <FeaturedProductCard key={item._id} item={item} />
       ))}
