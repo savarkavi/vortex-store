754664d2dc5e0668cfbed1ee5758543a2e661e90
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-03-27 09:55:33 +0530

    Add utility functions for product variant selection and stock checking

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,37 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { products } from "@wix/stores";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const findVariant = (
+  product: products.Product,
+  productOptions: Record<string, string>,
+) => {
+  if (!product.manageVariants) return null;
+
+  return (
+    product.variants?.find((item) =>
+      Object.entries(productOptions).every(
+        ([Key, value]) => item.choices?.[Key] === value,
+      ),
+    ) || null
+  );
+};
+
+export const checkInStock = (
+  product: products.Product,
+  productOptions: Record<string, string>,
+) => {
+  const variant = findVariant(product, productOptions);
+  console.log(variant);
+
+  return variant
+    ? variant.stock?.quantity !== 0 || variant.stock?.inStock
+    : product.stock?.quantity !== 0 ||
+        product.stock?.inventoryStatus === products.InventoryStatus.IN_STOCK ||
+        product.stock?.inventoryStatus ===
+          products.InventoryStatus.PARTIALLY_OUT_OF_STOCK;
+};
