3db7d400dc72cadc458135732bfe683f4725379d
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-03-29 12:00:02 +0530

    Enhance ProductImageCarousel to support automatic scrolling based on selected color option

--- src/components/product-page/ProductImageCarousel.tsx
+++ src/components/product-page/ProductImageCarousel.tsx
@@ -1,4 +1,5 @@
 import { products } from "@wix/stores";
+import { type CarouselApi } from "@/components/ui/carousel";
 
 import Image from "next/image";
 
@@ -9,13 +10,37 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { useEffect, useState } from "react";
 
 interface ProductImageCarouselProps {
   product: products.Product;
+  selectedOptions: Record<string, string>;
 }
-const ProductImageCarousel = ({ product }: ProductImageCarouselProps) => {
+const ProductImageCarousel = ({
+  product,
+  selectedOptions,
+}: ProductImageCarouselProps) => {
+  const [api, setApi] = useState<CarouselApi>();
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    const mediaIndex = product.media?.items?.findIndex((item) =>
+      item.title?.includes(selectedOptions?.Color?.toLowerCase()),
+    );
+
+    if (mediaIndex) {
+      api.scrollTo(mediaIndex);
+    }
+  }, [api, selectedOptions, product.media?.items]);
+
   return (
-    <Carousel className="w-full max-w-[550px] border">
+    <Carousel
+      className="sticky top-[112px] w-full max-w-[550px] shrink-0"
+      setApi={setApi}
+    >
       <CarouselContent className="h-[800px] w-full">
         {product.media?.items?.map((item) => (
           <CarouselItem key={item._id} className="relative h-full w-full">
