f4607e05cb73554bbb87346edc75bca2d9a3b9aa
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-04-02 07:42:33 +0530

    Refactor Navbar component to include scroll detection and dynamic styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,53 @@
+"use client";
+
 import Image from "next/image";
 import logoLight from "../assets/logo-light.png";
 import { SearchIcon, ShoppingCart, UserRound } from "lucide-react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
-const Navbar = async () => {
+const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("return", handleScroll);
+  }, [isScrolled]);
+
   return (
-    <div className="sticky top-0 mx-auto flex h-[80px] w-full max-w-[1680px] items-center justify-between p-4">
-      <div className="flex items-center gap-16">
-        <Link href="/">
-          <Image src={logoLight} alt="logo" width={48} height={48} />
-        </Link>
-        <div className="hidden items-center gap-10 xl:flex">
-          <p className="text-xl uppercase">Tops</p>
-          <p className="text-xl uppercase">Bottoms</p>
-          <p className="text-xl uppercase">Cyberpunk</p>
-          <p className="text-xl uppercase">More</p>
+    <div
+      className={cn(
+        "navbar sticky top-0 z-[10] h-[80px] w-full bg-[#fff] p-4",
+        isScrolled && "shadow-md transition-all",
+      )}
+    >
+      <div className="mx-auto flex max-w-[1680px] items-center justify-between">
+        <div className="flex items-center gap-16">
+          <Link href="/">
+            <Image src={logoLight} alt="logo" width={48} height={48} />
+          </Link>
+          <div className="hidden items-center gap-10 xl:flex">
+            <p className="text-xl uppercase">Tops</p>
+            <p className="text-xl uppercase">Bottoms</p>
+            <p className="text-xl uppercase">Cyberpunk</p>
+            <p className="text-xl uppercase">More</p>
+          </div>
         </div>
-      </div>
-      <div className="flex items-center gap-10">
-        <UserRound className="size-6" />
-        <SearchIcon className="size-6" />
-        <ShoppingCart className="size-6" />
+        <div className="flex items-center gap-10">
+          <UserRound className="size-6" />
+          <SearchIcon className="size-6" />
+          <ShoppingCart className="size-6" />
+        </div>
       </div>
     </div>
   );
